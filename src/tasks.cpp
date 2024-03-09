c5cab5b0533aa3ff37bddceaaf0adc8bec58c711
Andrey Chuvashov <ch.andrey.2345@gmail.com>
2024-03-10 01:26:23 +0300

    first commit

---
+++ src/tasks.cpp
@@ -0,0 +1,18 @@
+// Copyright 2024 Chuvashov Andrey
+#include "tasks.h"
+
+double RopeTask(double extraLength) {
+    const double eRadius = 6378.1;
+    Circle oldCircleLength = Circle(eRadius);
+    Circle newCircleLength = Circle(eRadius + extraLength / 1000);
+    return oldCircleLength.getArea() - newCircleLength.getArea();
+}
+
+double SwimmingPoolTask(double poolRadius, double roadWidth) {
+    double roadPrice = 1000, fencePrice = 2000;
+    Circle poolCircle = Circle(poolRadius);
+    Circle poolAndRoadCircle = Circle(poolRadius + roadWidth);
+    fencePrice *= poolAndRoadCircle.getFerence();
+    roadPrice *= (poolAndRoadCircle.getArea() - poolCircle.getArea());
+    return roadPrice + fencePrice;
+}
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,6 +1,147 @@
-
+// Copyright 2024 Chuvashov Andrey
 #include <gtest/gtest.h>
 #include <cstdint>
-#include "circle.h"
+#include "tasks.h"
 
+const double PI = 3.14159;
 
+TEST(CircleTests, checkNoRadius) {
+    Circle c;
+    EXPECT_DOUBLE_EQ(c.getRadius(), 0.0);
+    EXPECT_DOUBLE_EQ(c.getFerence(), 0.0);
+    EXPECT_DOUBLE_EQ(c.getArea(), 0.0);
+}
+
+TEST(CircleTests, checkSmallRadius) {
+    Circle c = Circle(7.0);
+    EXPECT_DOUBLE_EQ(c.getRadius(), 7.0);
+    EXPECT_DOUBLE_EQ(c.getFerence(), 7.0 * PI * 2);
+    EXPECT_DOUBLE_EQ(c.getArea(), 7.0 * 7.0 * PI);
+}
+
+TEST(CircleTests, checkHugeRadius) {
+    Circle c = Circle(6810.14);
+    EXPECT_DOUBLE_EQ(c.getRadius(), 6810.14);
+    EXPECT_DOUBLE_EQ(c.getFerence(), 6810.14 * PI * 2);
+    EXPECT_DOUBLE_EQ(c.getArea(), 6810.14 * 6810.14 * PI);
+}
+
+TEST(CircleTests, checkNegativeRadius) {
+    EXPECT_ANY_THROW(Circle::Circle(-1));
+}
+
+TEST(CircleTests, checkSetZeroRadius) {
+    Circle c;
+    c.setRadius(0.0);
+    EXPECT_DOUBLE_EQ(c.getRadius(), 0.0);
+    EXPECT_DOUBLE_EQ(c.getFerence(), 0.0);
+    EXPECT_DOUBLE_EQ(c.getArea(), 0.0);
+}
+
+TEST(CircleTests, checkSetSmallRadius) {
+    Circle c;
+    c.setRadius(11.0);
+    EXPECT_DOUBLE_EQ(c.getRadius(), 11.0);
+    EXPECT_DOUBLE_EQ(c.getFerence(), 11.0 * PI * 2);
+    EXPECT_DOUBLE_EQ(c.getArea(), 11.0 * 11.0 * PI);
+}
+
+TEST(CircleTests, checkSetHugeRadius) {
+    Circle c;
+    c.setRadius(1429.3);
+    EXPECT_DOUBLE_EQ(c.getRadius(), 1429.3);
+    EXPECT_DOUBLE_EQ(c.getFerence(), 1429.3 * PI * 2);
+    EXPECT_DOUBLE_EQ(c.getArea(), 1429.3 * 1429.3 * PI);
+}
+
+TEST(CircleTests, checkSetNegativeRadius) {
+    EXPECT_ANY_THROW(Circle::Circle().setRadius(-1.0));
+}
+
+TEST(CircleTests, checkSetZeroFerence) {
+    Circle c;
+    c.setFerence(0.0);
+    EXPECT_DOUBLE_EQ(c.getFerence(), 0.0);
+    EXPECT_DOUBLE_EQ(c.getRadius(), 0.0);
+    EXPECT_DOUBLE_EQ(c.getArea(), 0.0);
+}
+
+TEST(CircleTests, checkSetSmallFerence) {
+    Circle c;
+    c.setFerence(19.0);
+    EXPECT_DOUBLE_EQ(c.getFerence(), 19.0);
+    EXPECT_DOUBLE_EQ(c.getRadius(), 19.0 / (2 * PI));
+    EXPECT_DOUBLE_EQ(c.getArea(), 19.0 * (19.0 / (2 * PI)) / 2);
+}
+
+TEST(CircleTests, checkSetHugeFerence) {
+    Circle c;
+    c.setFerence(95678.193);
+    EXPECT_DOUBLE_EQ(c.getFerence(), 95678.193);
+    EXPECT_DOUBLE_EQ(c.getRadius(), 95678.193 / (2 * PI));
+    EXPECT_DOUBLE_EQ(c.getArea(), 95678.193 * (95678.193 / (2 * PI)) / 2);
+}
+
+TEST(CircleTests, checkSetNegativeFerence) {
+    EXPECT_ANY_THROW(Circle::Circle().setFerence(-1.0));
+}
+
+TEST(CircleTests, checkSetZeroArea) {
+    Circle c;
+    c.setArea(0.0);
+    EXPECT_DOUBLE_EQ(c.getArea(), 0.0);
+    EXPECT_DOUBLE_EQ(c.getRadius(), 0.0);
+    EXPECT_DOUBLE_EQ(c.getFerence(), 0.0);
+}
+
+TEST(CircleTests, checkSetSmallArea) {
+    Circle c;
+    c.setArea(123.4);
+    EXPECT_DOUBLE_EQ(c.getArea(), 123.4);
+    EXPECT_DOUBLE_EQ(c.getRadius(), sqrt(123.4 / PI));
+    EXPECT_DOUBLE_EQ(c.getFerence(), sqrt(123.4 / PI) * 2 * PI);
+}
+
+TEST(CircleTests, checkSetHugeArea) {
+    Circle c;
+    c.setArea(11351.151);
+    EXPECT_DOUBLE_EQ(c.getArea(), 11351.151);
+    EXPECT_DOUBLE_EQ(c.getRadius(), sqrt(11351.151 / PI));
+    EXPECT_DOUBLE_EQ(c.getFerence(), sqrt(11351.151 / PI) * 2 * PI);
+}
+
+TEST(CircleTests, checkSetNegativeArea) {
+    EXPECT_ANY_THROW(Circle::Circle().setArea(-1.0));
+}
+
+TEST(TasksTests, RopeTaskNegativeParams) {
+    EXPECT_ANY_THROW(RopeTask(-1));
+}
+
+TEST(TasksTests, RopeTaskSmall) {
+    EXPECT_EQ(RopeTask(1.0), 40.07475);
+}
+
+TEST(TasksTests, RopeTaskMiddle) {
+    EXPECT_EQ(RopeTask(100.0), 4007.50645);
+}
+
+TEST(TasksTests, RopeTaskHuge) {
+    EXPECT_EQ(RopeTask(10000.0), 401061.66258);
+}
+
+TEST(TasksTests, SwimmingPoolTaskNegativeParams) {
+    EXPECT_ANY_THROW(SwimmingPoolTask(-1.0, -1.0));
+}
+
+TEST(TasksTests, SwimmingPoolTaskSmall) {
+    EXPECT_EQ(SwimmingPoolTask(3.0, 1.0), 72256.57);
+}
+
+TEST(TasksTests, SwimmingPoolTaskMiddle) {
+    EXPECT_EQ(SwimmingPoolTask(60.0, 3.0), 1950927.39);
+}
+
+TEST(TasksTests, SwimmingPoolTaskHuge) {
+    EXPECT_EQ(SwimmingPoolTask(90.0, 5.0), 4099774.95);
+}
